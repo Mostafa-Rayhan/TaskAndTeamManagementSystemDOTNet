e87051de114b04a50fb3f3835496848d87b81ab2
Mostafa Rayhan <mostafarayhan57@gmail.com>
2025-12-17 15:01:34 +0600

    middleware and fluint validation

--- TaskManagementSystem.API/Middleware/ExceptionHandlingMiddleware.cs
+++ TaskManagementSystem.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,6 +1,47 @@
-﻿namespace TaskManagementSystem.API.Middleware
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace TaskManagementSystem.API.Middleware
 {
     public class ExceptionHandlingMiddleware
     {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An unhandled exception occurred");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error",
+                Detail = exception.Message
+            };
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
     }
 }
---
+++ TaskManagementSystem.API/Validators/UserValidator.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation;
+using TaskManagementSystem.Core.Entities;
+
+namespace TaskManagementSystem.API.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.FullName)
+                .NotEmpty().WithMessage("Full name is required")
+                .MaximumLength(100).WithMessage("Full name cannot exceed 100 characters");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format")
+                .MaximumLength(100).WithMessage("Email cannot exceed 100 characters");
+
+            RuleFor(x => x.Role)
+                .IsInEnum().WithMessage("Invalid role");
+        }
+    }
+
+    public class TaskValidator : AbstractValidator<Task>
+    {
+        public TaskValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required")
+                .MaximumLength(200).WithMessage("Title cannot exceed 200 characters");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(1000).WithMessage("Description cannot exceed 1000 characters");
+
+            RuleFor(x => x.DueDate)
+                .GreaterThan(DateTime.UtcNow).WithMessage("Due date must be in the future");
+
+            RuleFor(x => x.Status)
+                .IsInEnum().WithMessage("Invalid status");
+        }
+    }
+
+    public class TeamValidator : AbstractValidator<Team>
+    {
+        public TeamValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .MaximumLength(100).WithMessage("Name cannot exceed 100 characters");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Description cannot exceed 500 characters");
+        }
+    }
+}
